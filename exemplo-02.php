29d0fbdca79b0ba624e0362e8ed4411c89f434e6
Fernando dos Santos Muethea <64161845+Muethea@users.noreply.github.com>
2022-06-01 13:51:35 +0100

    Variaveis

---
+++ exemplo-02.php
@@ -0,0 +1,15 @@
+<?php
+
+$anoNascimento = 1990;
+$nomeCompleto = "Maria";
+
+$numero1 =2;
+$numero2 = 5;
+$resultado = $numero1 + $numero2;
+echo $resultado;
+
+
+//echo $anoNascimento . " ". $nomeCompleto;
+
+
+?>
---
+++ variaveis.php
@@ -0,0 +1,25 @@
+<?php
+
+$nome = "Fernando";
+$site = 'www.hcode.com.br';
+
+$ano = 1998;
+$salario = 6000.8;
+$bloquado = false;
+
+echo "Nome: ".$nome." </br>"." ".$site ." ".$ano;
+
+//////////////////////////////////
+
+$nomes = array("Fernando", "Maria", "Manuela", "Luis");
+
+"</br>";
+$nascimento = new DateTime();
+//var_dump($nascimento);
+
+////////////////////////////////////////////////
+
+$arquivo = fopen("variaveis.php", "r");
+
+var_dump($arquivo);
+?>
